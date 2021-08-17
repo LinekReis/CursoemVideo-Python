86458e51d497f0263537661ab6f708d1816bc07d
Linek Reis <linekerreis12@hotmail.com>
2021-08-17 17:38:03 -0300

    Update Exercício 59.py

--- World2/Exercício 59.py
+++ World2/Exercício 59.py
@@ -1,5 +1,5 @@
 firstnumber = int(input("Tipe the first number: "))
-second = int(input("Tipe the second number: "))
+secondnumber = int(input("Tipe the second number: "))
 
 option = 0
 
@@ -7,6 +7,9 @@ while option != 5:
     print("[1] Sum the two values\n[2] Multiply")
     print("[3] Greatest\n[4] New values\n[5] Exit program")
     option = int(input("Tipe the disered option: "))
-    if option == 4:
+    if option ==1:
+        result = firstnumber+secondnumber
+        print("The sum of the two values is equal to: {}".format(result))
+    elif option == 4:
         firstnumber = int(input("Tipe the first number: "))
         second = int(input("Tipe the second number: "))
