02c5b9c1126c3fa957d86ab1d28af7ff64d19b8d
Linek Reis <linekerreis12@hotmail.com>
2021-08-23 19:24:04 -0300

    Update Exercício 70.py

--- World2/Exercício 70.py
+++ World2/Exercício 70.py
@@ -1,17 +1,17 @@
-cheapest = product = ''
-amount = value = higher1000= 0
+cheapestname = product = ''
+amount = cheapest = value = higher1000= 0
 higher1000 = 0
 while True: 
    product = str(input("Type the name pf the product: "))
    value = int(input("Value of that product: "))
+   cheapest = value
    amount += value 
    if value > 1000:
      higher1000 += 1
    if value < cheapest:
-     cheapest = product
+     cheapestname = product
    print("Press any button to continue\nOr type 'No' to end the program") 
    resp = str(input("Do you want to continue ? ")).upper()
-   while resp != 'NO':
-       print("Press any button to continue\nOr type 'No' to end the program") 
-       resp = str(input("Do you want to continue ? ")).upper 
-print(f"Quantity of products that costs\nmore or equal than 1000 {higher1000}")
+   if resp == 'NO':
+     break 
+print(f"Quantity of products that costs\nmore or equal than U$ 1000 {higher1000}")
