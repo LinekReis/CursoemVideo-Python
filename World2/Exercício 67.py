3b1e48a86f1c50102f6422ca7e59aace1f8b46af
Linek Reis <linekerreis12@hotmail.com>
2021-08-23 15:04:13 -0300

    Update Exercício 67.py

--- World2/Exercício 67.py
+++ World2/Exercício 67.py
@@ -1,10 +1,11 @@
-
+import os 
 print("Times Table V3.0")
 print("-"*16)
 
 while True:
     print("Tipe any negative value to end the program")
     number = int(input("Wich number do you want to know the times table: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
     if number < 0:
         break
     for c in range (0,11):
