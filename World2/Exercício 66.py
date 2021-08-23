214da81b93f6c4634651fa3dff61a4557ec07a8c
Linek Reis <linekerreis12@hotmail.com>
2021-08-23 16:37:15 -0300

    Update Exercício 66.py

--- World2/Exercício 66.py
+++ World2/Exercício 66.py
@@ -5,9 +5,9 @@ number = total = count =0
 print("999 will break the looping")
 
 while number != 999:
-    number=int(input("Tipe a number: "))
+    number=int(input("Type a number: "))
     if number == 999:
         break
     total += number
     count += 1     
-print("Sum of the numbers: {}\nQuantity of numbers tiped: {}".format(total,count))
+print("Sum of the numbers: {}\nQuantity of numbers typed: {}".format(total,count))
