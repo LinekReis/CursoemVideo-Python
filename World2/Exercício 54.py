22383f7b6710e06fd26fc9ea79d3872460b21305
Linek Reis <linekerreis12@hotmail.com>
2021-08-23 16:40:42 -0300

    Update Exercício 54.py

--- World2/Exercício 54.py
+++ World2/Exercício 54.py
@@ -6,7 +6,7 @@ count = 0
 count2 = 0
 
 for i in range(0,7):
-  birth = int(input("Tipe the year of your birthday: "))
+  birth = int(input("Type the year of your birthday: "))
   age = today - birth
     if age >= 21:
         count += 1
