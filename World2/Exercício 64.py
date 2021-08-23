d8e25b53dbb939182eac23bf9bfe65a3b82fc5c9
Linek Reis <linekerreis12@hotmail.com>
2021-08-23 16:38:15 -0300

    Update Exercício 64.py

--- World2/Exercício 64.py
+++ World2/Exercício 64.py
@@ -7,26 +7,26 @@ total = 0
 count = 0
 
 while number != 999:
-    number=int(input("Tipe a number: "))
+    number=int(input("Type a number: "))
     total += number
     count += 1 
     if number == 999:
         total -= 999
         count -= 1
-print("Sum of the numbers: {}\nQuantity of numbers tiped: {}".format(total,count))
+print("Sum of the numbers: {}\nQuantity of numbers typed: {}".format(total,count))
 
 ## option 2 ---
 print("Working with Numbers")
 print("-"*20)
 
 number = count = total = 0
-number=int(input("Tipe a number: "))
+number=int(input("Type a number: "))
 
 while number != 999:
     total += number
     count += 1 
-    number=int(input("Tipe a number: "))
-print("Sum of the numbers: {}\nQuantity of numbers tiped: {}".format(total,count))
+    number=int(input("Type a number: "))
+print("Sum of the numbers: {}\nQuantity of numbers typed: {}".format(total,count))
 
 ## option 3 ---
 print("Working with Numbers")
@@ -35,9 +35,9 @@ print("-"*20)
 number = total = count =0
 
 while number != 999:
-    number=int(input("Tipe a number: "))
+    number=int(input("Type a number: "))
     if number == 999:
         break
     total += number
     count += 1     
-print("Sum of the numbers: {}\nQuantity of numbers tiped: {}".format(total,count))
+print("Sum of the numbers: {}\nQuantity of numbers typed: {}".format(total,count))
